409e90c280ec6cae79fb6f4479844437eef105d9
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2020-01-10 11:33:04 +0000

    insertionSort in ascending order

--- src/logic/sortingAlgorithms.js
+++ src/logic/sortingAlgorithms.js
@@ -1,106 +1,3 @@
-function mergeSort(
-  array,
-  swapBars,
-  lowerBound = 0,
-  upperBound = array.length - 1,
-  auxiliaryArray = [...array]
-) {
-  // lowerBound = lowerBound === undefined ? 0 : lowerBound;
-  // upperBound = upperBound === undefined ? array.length - 1 : upperBound;
-
-  const middleIndex = Math.floor((upperBound + lowerBound) / 2);
-
-  // if (leftLength <= 1 || rightLength <=1) {
-  if (upperBound - lowerBound <= 1) {
-    console.log("returning", array.slice(lowerBound, upperBound));
-    return {
-      array,
-      lowerBound,
-      upperBound
-    };
-  }
-
-  return merge(
-    array,
-    swapBars,
-    mergeSort(array, swapBars, lowerBound, middleIndex, auxiliaryArray),
-    mergeSort(array, swapBars, middleIndex, upperBound, auxiliaryArray),
-    auxiliaryArray
-  );
-}
-
-function merge(
-  array,
-  swapBars,
-  { lowerBound: leftLower, upperBound: leftUpper },
-  { lowerBound: rightLower, upperBound: rightUpper },
-  auxiliaryArray
-) {
-  console.log(
-    "merging",
-    array.slice(leftLower, leftUpper),
-    array.slice(rightLower, rightUpper)
-  );
-
-  console.log(leftLower, rightLower);
-
-  let index = leftLower;
-  let leftIndex = leftLower;
-  let rightIndex = rightLower;
-
-  let hasBeenSwapped = [];
-  //use aux when comparing
-  while (leftIndex < leftUpper && rightIndex < rightUpper) {
-    console.log(
-      array[leftIndex],
-      leftIndex,
-      "<",
-      array[rightIndex],
-      rightIndex
-    );
-    if (auxiliaryArray[leftIndex] < auxiliaryArray[rightIndex]) {
-      // console.log("swapping", array[index], array[leftIndex]);
-      // console.log("swapping")
-      swap(array, index, leftIndex);
-      swapBars([index, leftIndex]);
-      leftIndex++;
-      index++;
-    } else {
-      // console.log("swapping", index, rightIndex)
-      swap(array, index, rightIndex);
-      swapBars([index, rightIndex]);
-      rightIndex++;
-      index++;
-    }
-  }
-
-  console.log("left", leftUpper - leftIndex);
-  console.log("right", rightUpper - rightIndex);
-
-  // while(leftIndex < leftUpper){
-  //   console.log(index, leftIndex)
-  //   swap(array, index, leftIndex)
-  //   swapBars([index, leftIndex]);
-
-  //   index++;
-  //   leftIndex++;
-  // }
-
-  // while(rightIndex < rightUpper){
-  //   console.log(index, rightIndex)
-  //   swap(array, index, rightIndex);
-  //   swapBars([index, rightIndex]);
-
-  //   index++;
-  //   rightIndex++;
-  // }
-
-  return {
-    array,
-    lowerBound: leftLower,
-    upperBound: rightUpper
-  };
-}
 
 function insertionSort(array, swapBars) {
   // const length = array.length;
@@ -113,7 +10,7 @@ function insertionSort(array, swapBars) {
     for (let secondIndex = index -1; secondIndex >= 0; secondIndex--) {
       const elementToCompare = array[secondIndex];
       
-      if(elementToCompare < element){
+      if(elementToCompare > element){
 
         swap(array, secondIndex, secondIndex + 1);
         swapBars([secondIndex, secondIndex + 1])
@@ -246,7 +143,6 @@ function bubbleSort(array, swapBars) {
 }
 
 const sortingAlgorithms = {
-  mergeSort,
   insertionSort,
   quickSort,
   heapSort,
